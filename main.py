12a1b5b61ae1ca779eb6b197f27456f84caee827
soloiaros <soloiaros@gmail.com>
2024-04-04 23:20:27 +0300

    new extendable html added, header added

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print('Начало проекта!')
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '123'
+
+
+@app.route('/')
+def main_page():
+    return render_template('main_page.html')
+
+
+if __name__ == '__main__':
+    app.run(port=8000, host='127.0.0.1')
