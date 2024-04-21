f3695d5b2eeaa261146f3cc2bb68dfeb6365e415
oxreng <merkurev06@mail.ru>
2024-04-21 15:00:25 +0300

    translated from rus -> eng

--- forms/register.py
+++ forms/register.py
@@ -4,10 +4,10 @@ from wtforms.validators import DataRequired
 
 
 class RegisterForm(FlaskForm):
-    name = StringField('Ваше имя', validators=[DataRequired()])
+    name = StringField('Your name', validators=[DataRequired()])
     email = EmailField('Email', validators=[DataRequired()])
-    password = PasswordField('Придумайте пароль', validators=[DataRequired()])
-    password_again = PasswordField('Повторите пароль', validators=[DataRequired()])
-    bday = DateField('День рождения')
-    remember_me = BooleanField('Запомнить меня')
-    submit = SubmitField('Создать аккаунт')
+    password = PasswordField('Password', validators=[DataRequired()])
+    password_again = PasswordField('Repeat password', validators=[DataRequired()])
+    bday = DateField('Your Birthday')
+    remember_me = BooleanField('Remember me')
+    submit = SubmitField('Create an account')
